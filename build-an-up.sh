47224acdcf0af1cd9ec2f1b659e24ca72ab3b32e
Alessandro Bruno <67830217+alessandrobrunoh@users.noreply.github.com>
2025-07-20 10:38:09 +0200

    Create build-an-up.sh

---
+++ build-an-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Building Docker image from Dockerfile..."
+docker build -t kafka-api .
+
+echo "Stopping and removing existing Docker containers..."
+docker-compose down --rmi all -v --remove-orphans
+
+echo "Starting Docker Compose..."
+docker-compose up --build
+
